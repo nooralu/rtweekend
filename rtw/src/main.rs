6e6d69f49f67f82ee4a651991c155dafaef88517
nooralu <nooralu@foxmail.com>
2023-05-01 21:05:42 +0800

    2.3 Adding a Progress Indicator

--- rtw/src/main.rs
+++ rtw/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     print!("P3\n{image_width} {image_height}\n255\n");
 
     for j in (0..image_height).rev() {
+        eprintln!("\rScanlines remaining: {j}");
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -17,4 +18,5 @@ fn main() {
             println!("{ir} {ig} {ib}");
         }
     }
+    eprintln!("Done.");
 }
